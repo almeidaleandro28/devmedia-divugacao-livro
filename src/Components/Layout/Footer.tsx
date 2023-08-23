110dc7bf59065ddb2279150fcbbbeebeac0acee0
leandro almeida da silva <almeidaleandro.silva@gmail.com>
2023-08-23 11:58:27 -0300

    feat: add footer

--- src/Components/Layout/Footer.tsx
+++ src/Components/Layout/Footer.tsx
@@ -3,7 +3,7 @@ import Button from "../Input/Button";
 function Footer(props) {
   return (
     <footer className="">
-      <div className="container">
+      <div className="container mx-auto text-center">
         
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, voluptates accusamus? Sint illo omnis libero sunt facere est incidunt consequuntur, reprehenderit maiores accusantium deserunt ratione.
