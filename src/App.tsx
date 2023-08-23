d5918dbee246ab4af3a8f6b9f57952f074217b97
leandro almeida da silva <almeidaleandro.silva@gmail.com>
2023-08-23 11:58:47 -0300

    feat: add Section

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Footer from "./Components/Layout/Footer"
 import Header from "./Components/Layout/Header"
+import Section from "./Components/Layout/Sections"
 
 function App() {
   return (
     <>
       <Header />
+      <Section />
       <Footer />
     </>
   )
---
+++ src/Components/Layout/Sections.tsx
@@ -0,0 +1,15 @@
+
+function Section(props) {
+  return (
+    <section className="border-b-2">
+      <div className="container mx-auto text-center">
+      <h3 className="font-bold text-lg">an intense story</h3>
+      <p className="">
+        <span className="font-bold">Artificial age - the dark time of machines</span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque culpa animi tenetur iste aut nobis?
+      </p>
+      </div>
+    </section>
+  );
+}
+
+export default Section;
