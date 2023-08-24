72f3b95687c416e94573e18c877ad5fbef55f64c
leandro almeida da silva <almeidaleandro.silva@gmail.com>
2023-08-24 11:09:16 -0300

    add cursor for buttons

--- src/Components/Input/Button.tsx
+++ src/Components/Input/Button.tsx
@@ -6,7 +6,8 @@ function Button( { msg }: options) {
   return (
     <button className="mt-4 bg-green-500 text-white font-bold uppercase 
         py-2 px-5 rounded-md
-        focus:ring-green-300 hover:bg-green-700"
+        focus:ring-green-300 hover:bg-green-700
+        cursor-pointer"
                   >{ msg }</button>
   );
 }
