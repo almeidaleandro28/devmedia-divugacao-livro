ff983f9e477e203f652b2eb5dae06e73b7762f62
leandro almeida da silva <almeidaleandro.silva@gmail.com>
2023-08-24 10:55:06 -0300

    style: add style for btns

--- src/Components/Input/Button.tsx
+++ src/Components/Input/Button.tsx
@@ -4,7 +4,10 @@ type options  = {
 
 function Button( { msg }: options) {
   return (
-    <button className="bg-green-500 text-white font-bold uppercase py-2 px-5 rounded-md">{ msg }</button>
+    <button className="mt-4 bg-green-500 text-white font-bold uppercase 
+        py-2 px-5 rounded-md
+        focus:ring-green-300 hover:bg-green-700"
+                  >{ msg }</button>
   );
 }
 
