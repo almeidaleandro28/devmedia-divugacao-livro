507ff0385fd4aa2fcc8efc5a5dad3dc7b1575993
leandro almeida da silva <almeidaleandro.silva@gmail.com>
2023-08-24 10:22:44 -0300

    feat: create about section

---
+++ src/Components/Layout/About.tsx
@@ -0,0 +1,20 @@
+import photo from '../../assets/autor.jpg';
+
+function About(props) {
+  return (
+    <section className='border-b-2'>
+      <div className="container mx-auto text-center">
+        
+        <h3>about the author</h3>
+
+        <img className='mx-auto' src={photo} alt="perfil photo" />
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro corrupti dignissimos possimus nemo eaque? Beatae!
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default About;
